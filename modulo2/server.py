e899c161f77914391163549433b947cbe1cded0b
Jorge Rios <jorge.rios@admins-MacBook-Pro-8.local>
2020-07-25 12:06:46 -0500

    first commit

--- modulo2/server.py
+++ modulo2/server.py
@@ -1,17 +1,17 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
-#import mariadb
-#conn = mariadb.connect(user="jorge.rios", password="Macros3", host="localhost", port=3306, database="proyectox")
-#cur = conn.cursor()
+import mariadb
+conn = mariadb.connect(user="jorge.rios", password="Macros3", host="localhost", port=3306, database="proyectox")
+cur = conn.cursor()
 
-#import sqlalchemy
-#from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 
 
 # Test if it works
-#engine = sqlalchemy.create_engine('mysql+mysqldb://jorge.rios:Macros3@localhost:3306/proyectox')
-#conn = engine.connect().connection
-#session= sessionmaker(bind =engine)()
+engine = sqlalchemy.create_engine('mysql+mysqldb://jorge.rios:Macros3@localhost:3306/proyectox')
+conn = engine.connect().connection
+session= sessionmaker(bind =engine)()
 #cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 #cors = CORS(app, resources={r"/auth": {"origins": "*"}})
 #cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -21,26 +21,40 @@ app.debug = True
 @app.route('/', methods=['GET'])
 def main():
     #mariadbdriver
-    #cur.execute("select nombre as nombre from personas")
+    #cur.execute("select nombre, edad from personas")
     #headers = cur.description
+    #print("viendo headers ",headers[0][0], headers[1][0])
     #vals = cur.fetchall()
     #for x in vals:
-    #    print(x)
+    #    print(x.edad)
 
     #sqlalchemy
-    #for x in session.execute("select nombre from personas"):
-    #    print(x)
+    print("sqlalchemy")
+    usuarios = []
+    valores = {}
+    for x in session.execute("select id as id, nombre as nombre, edad as edad, apellido_paterno as ap from personas"):
+        usuarios.append(dict(id = x.id, nombre=x.nombre, edad=x.edad, apaterno=x.ap))
 
     
-    return jsonify({"msg": "hola mundo"}), 200
-    #return render_template('index.html')
+    #return jsonify({"usuarios": usuarios}), 200
+    valores["usuarios"]= usuarios
+    valores["titulo"] = "todos los usuarios"
+    return render_template('index.html', valores=valores)
 
 
-@app.route('/usuario', methods=['GET'])
+@app.route('/usuario', methods=['GET', 'POST', 'PUT'])
 def usuario():
     if request.method == "POST":
+        #print("entro en metodo post")
         print(request.form)
-    return render_template('index.html')
+        nombre = request.form.get("nombre")
+        edad = request.form.get("edad")
+        session.execute("insert into personas (nombre, edad) values ('{}', '{}')".format(nombre, edad))
+        session.commit()
+    if request.method == "PUT":
+        pass
+
+    return render_template('forma.html')
 
 
 
