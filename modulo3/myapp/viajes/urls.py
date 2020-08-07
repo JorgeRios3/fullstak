fa6c58f5e627f321fe174971fc505d9f693dfa65
Jorge Rios <jorge.rios@admins-MacBook-Pro-8.local>
2020-08-07 14:06:46 -0500

    django authentication

--- modulo3/myapp/viajes/urls.py
+++ modulo3/myapp/viajes/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from . import views
+from . import views as otra
+
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('viaje/<nombre>/', views.bio, name='bio'),
-    path('persona/', views.forma_viaje, name='bio'),
-    path('personas/', views.PersonaLista.as_view())
+    path('', otra.index, name='index'),
+    path('viaje/<nombre>/', otra.bio, name='bio'),
+    path('persona', otra.forma_viaje, name='persona'),
+    path('personas', otra.PersonaLista.as_view()),
+    path('login', otra.login, name='login'),
+    path('logout', otra.logout, name='logout'),
+    path('vista', otra.Vista.as_view(), name="vista")
 ]
--- modulo3/myapp/viajes/views.py
+++ modulo3/myapp/viajes/views.py
@@ -6,6 +6,19 @@ from sqlalchemy.orm import sessionmaker
 from . import models
 from .forms import NameForm
 from django.views.generic import ListView
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+
+
+from django.contrib.auth import authenticate, login as login_django, logout as logout_django
+from django.contrib.auth.decorators import permission_required, login_required
+
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+
+
 
 
 
@@ -27,23 +40,33 @@ def index(request):
     #persona.nombre="nuevo nombre"
     #persona.save()
     #persona.delete()
-    [vals] = models.Pago.objects.all().select_related('viaje')
-    print(vals.viaje.descripcion)
-    return HttpResponse("pagina principal")
+    #[vals] = models.Pago.objects.all().select_related('viaje')
+    #print(vals.viaje.descripcion)
+    #return HttpResponse("pagina principal")
+    if request.user.is_authenticated:
+        return render(request, "viajes/index.html")
+    return redirect('/login')
 
+#@login_required
+@login_required(login_url='/login')
+@permission_required(['viajes.view_viaje'])
 def bio(request, nombre="jorge"):
-    usuarios = []
-    for x in session.execute("select id as id, nombre as nombre, edad as edad, apellido_paterno as ap from personas"):
-        usuarios.append(dict(id = x.id, nombre=x.nombre, edad=x.edad, apaterno=x.ap))
-    print(request)
-    template = loader.get_template('viajes/index.html')
-    return HttpResponse(template.render({'nombre':nombre, 'apellido':'rios', 'usuarios':usuarios}, request))
+    #usuarios = []
+    #for x in session.execute("select id as id, nombre as nombre, edad as edad, apellido_paterno as ap from personas"):
+        #usuarios.append(dict(id = x.id, nombre=x.nombre, edad=x.edad, apaterno=x.ap))
+    #print(request)
+    if request.user.is_authenticated:
+        #template = loader.get_template('viajes/index.html')
+        #return HttpResponse(template.render({'nombre':nombre, 'apellido':'rios', 'usuarios':usuarios}, request))
+        return HttpResponse("estas en ruta viaje/....")
+    else:
+        return HttpResponse("no estas logeado")
 
 def forma_viaje(request, id=0):
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
-            nombre = request.POST.get("nombre"))
+            nombre = request.POST.get("nombre")
             #nombre = request.POST.get("apellido_paterno"))
             #p1 = Persona(nombre="jor")
             #p1.save()
@@ -51,11 +74,70 @@ def forma_viaje(request, id=0):
         else:
             return HttpResponseRedirect('/crear/')
     else:
-        print("aca")
+        is_superuser = request.user.is_superuser
         form = NameForm()
-
-    return render(request, 'persona.html', {'form': form})
+        if is_superuser == False:
+            form.base_fields['nombre'].disabled = True
+        return render(request, 'persona.html', {'form': form})
 
 
 class PersonaLista(ListView):
     model = models.Persona
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login_django(request, user)
+            usuarios = []
+            template = loader.get_template('viajes/index.html')
+            return HttpResponse(template.render({'nombre':'jorge', 'apellido':'rios', 'usuarios':usuarios}, request))
+        else:
+            return render(request, 'viajes/login.html', {})
+    else:
+        return render(request, 'viajes/login.html', {})
+
+
+def logout(request):
+    try:
+        logout_django(request)
+        return HttpResponseRedirect('/')
+    except:
+        return HttpResponse("error en logout")
+
+
+#@method_decorator(login_required, name='dispatch')
+#@method_decorator(permission_required("viajes.view_viaje"), name="dispatch")
+#@method_decorator(permission_required('is_staff'), name="dispatch")
+@method_decorator(csrf_exempt, name='dispatch')
+class Vista(View):
+    def get(self, request):
+        print("por el get")
+        is_superuser = request.user.is_superuser
+        form = NameForm()
+        if is_superuser == False:
+            form.base_fields['nombre'].disabled = True
+        return render(request, 'persona.html', {'form': form})
+
+    def post(self, request):
+        form = NameForm(request.POST)
+        if form.is_valid():
+            nombre = request.POST.get("nombre")
+            #nombre = request.POST.get("apellido_paterno"))
+            #p1 = Persona(nombre="jor")
+            #p1.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/crear/')
+
+    def put(self, request):
+        print("entro al put")
+        return HttpResponse("regreso metodo put")
+    
+    def delete(self, request):
+        print("entro en metodo delete")
+        return HttpResponse("regreso metodo delete")
+
